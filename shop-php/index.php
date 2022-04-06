3ee1e1768376ab77f02fe1455fc98a9bc9130f52
Alireza Rostami <80385218+20Alireza20@users.noreply.github.com>
2022-04-06 10:26:41 +0430

    Add files via upload

---
+++ shop-php/index.php
@@ -0,0 +1,7 @@
+﻿<?php
+	session_start();
+	include_once ('shop_sc_fns.php');
+	do_html_header('www.mspsoft.com ام اس پي سافت بزرگترين منبع پروژه هاي و سورس كدهاي برنامه نويسي',get_categories());
+	display_latest();
+	do_html_footer();
+?>
